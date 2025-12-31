7117d93276b89ad172238b0acd4298d750008474
catGirl_seeker <danildenisov572@gmail.com>
2026-01-01 00:13:09 +0500

    border buttons

--- app/components/Buttons.vue
+++ app/components/Buttons.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="px-10 flex gap-5 pb-5">
     <div v-for="button in buttons">
-      <div class="btn bg-accent border-0 py-3 px-6 rounded-3xl transition duration-700 hover:border">{{ button.text }}</div>
+      <div class="btn bg-accent border-transparent py-3 px-6 rounded-3xl transition duration-400 hover:border-base-200 border">{{ button.text }}</div>
     </div>
   </div>
 </template>
